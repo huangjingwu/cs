d4a60f663d211e61d3d3af33d188b3ea20e6188e
黄小武 <914267589@qq.com>
2020-05-07 19:17:49 +0800

    cs

--- demo.py
+++ demo.py
@@ -9,3 +9,25 @@ print("欢迎来到python学习！")
 
 
 
+print("欢迎来到python学习！")
+
+
+print("欢迎来到python学习！")
+print("欢迎来到python学习！")
+print("欢迎来到python学习！")
+print("欢迎来到python学习！")
+print("欢迎来到python学习！")
+
+
+
+print("欢迎来到python学习！")
+
+
+print("欢迎来到python学习！")
+print("欢迎来到python学习！")
+print("欢迎来到python学习！")
+print("欢迎来到python学习！")
+print("欢迎来到python学习！")
+
+
+
